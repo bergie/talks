a7b67a57677dfd8e63ee4fd7ce5094734f39ab4f
Henri Bergius <henri.bergius@iki.fi>
2014-05-14 00:49:39 +0200

    Initial jsDay slides

---
+++ 2014/noflo-jsday/talk.md
@@ -0,0 +1,77 @@
+---
+title: Flow-Based Programming for JavaScript
+location: Verona, Italy
+---
+Flow-Based Programming for JavaScript
+=====================================
+
+![NoFlo](../../assets/2013/noflo-white.svg)
+
+## Call-Return
+
+    var value = someFunction(param);
+    // Here we wait until this completes
+
+## Continuation Passing Style
+
+    someFunction(param, function (value) {
+      // Our callback gets called once the
+      // function completes
+    });
+
+## Callback Christmas Tree
+
+    asncFunction1(function(err, result) {
+      asncFunction2(function(err, result) {
+        asncFunction3(function(err, result) {
+          asncFunction4(function(err, result) {
+            asncFunction5(function(err, result) {
+              // do something useful
+            })
+          })
+        })
+      })
+    });
+
+![Underground map](../../assets/2013/underground-map.png)
+
+![J. Paul Morrison](../../assets/2013/paul-morrison.jpg)
+
+![The FBP book](../../assets/2013/fbp-book.png)
+
+![IBM Collator](../../assets/2014/collator.png)
+![IBM flowcharts](../../assets/2014/flowchart.png)
+
+![Alan Kay](../../assets/2013/alan-kay.png)
+
+> I thought of objects being like biological cells and/or individual computers on a network, only able to communicate with messages
+
+![Usenix](../../assets/2013/usenix.jpg)
+
+> This is the Unix philosophy: 
+Write programs that do one thing and do it well. 
+Write programs to work together. 
+Write programs to handle text streams, because that is a universal interface.
+      
+## Unix shell &#8594; FBP in 1D
+
+    $ ps -ef | grep firefox \
+      | grep -v grep \
+      | awk '{print $2}' \
+      | xargs kill -9
+
+![NoFlo roadtrip](../../assets/2013/noflo-california-1.jpg)
+
+![Jekyll built in NoFlo](../../assets/2014/noflo-jekyll.png)
+
+![NoFlo Photobooth](../../assets/2014/noflo-photobooth.png)
+
+![NoFlo Kickstarter](../../assets/2013/noflo-kickstarter-finished.png)
+
+![NoFlo and GNOME](../../assets/2014/noflo-gnome-ui.png)
+![MicroFlo simulator](../../assets/2014/microflo-sim1.gif)
+
+![Ingress Table](../../assets/2014/ingress-table-render.jpg)
+![Ingress Table debugging](../../assets/2014/ingress-table-test2.gif)
+
+![Flowhub public beta](../../assets/2014/flowhub-beta.png)
---
+++ assets/2014/slides.js
@@ -0,0 +1,134 @@
+(function () {
+  var currentSlide = 0;
+  function previous(slides) {
+    currentSlide--;
+    window.location.hash = '#' + currentSlide;
+  }
+  function next(slides) {
+    currentSlide++;
+    window.location.hash = '#' + currentSlide;
+  }
+  var scrolling = false;
+  var to;
+  function scroll(slide) {
+    var from = window.scrollY;
+    console.log(slide, JSON.stringify(slide.getBoundingClientRect()));
+    to = slide.getBoundingClientRect().top + window.pageYOffset;
+    var step = Math.floor(Math.abs(to - from) / 10);
+    var scrollStep = function () {
+      var next;
+      if (from >= to) {
+        next = from - step;
+        if (next <= to) {
+          window.scrollTo(0, to);
+          scrolling = false;
+          return;
+        }
+      } else {
+        next = from + step;
+        if (next > to) {
+          window.scrollTo(0, to);
+          scrolling = false;
+          return;
+        }
+      }
+      window.scrollTo(0, next);
+      window.requestAnimationFrame(scrollStep);
+      from = next;
+    };
+    window.requestAnimationFrame(scrollStep);
+  }
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var slides = document.querySelectorAll('section.slide');
+    if (!window.location.hash) {
+      window.location.hash = '#0';
+    }
+    setTimeout(function () {
+      processHash(window.location.hash);
+    }, 1000);
+    function processHash(hash) {
+      var parts = hash.split('#');
+      if (parts.length < 2) {
+        return;
+      }
+      currentSlide = parseInt(parts[1]);
+      if (isNaN(currentSlide)) {
+        window.location.hash = '#0';
+        return;
+      }
+      if (currentSlide >= slides.length) {
+        window.location.hash = '#' + (slides.length - 1);
+        return;
+      }
+      if (currentSlide < 0) {
+        window.location.hash = '#0';
+        return;
+      }
+      scroll(slides[currentSlide]);
+    };
+    window.addEventListener('hashchange', function (event) {
+      processHash(event.newURL);
+    });
+    window.addEventListener('resize', function (event) {
+      setTimeout(function () {
+        var top = slides[currentSlide].getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo(0, top);
+      }, 500);
+    });
+    document.addEventListener('keydown', function (event) {
+      if (event.keyCode === 38) {
+        event.preventDefault();
+        previous(slides);
+      }
+      if (event.keyCode === 40) {
+        event.preventDefault();
+        next(slides);
+      }
+    });
+    var delta;
+    var startPosition;
+    document.addEventListener('touchstart', function (event) {
+      if (event.touches.length !== 1) {
+        return;
+      }
+      event.preventDefault();
+      delta = 0;
+      startPosition = event.touches[0].pageY;
+    });
+    document.addEventListener('touchmove', function (event) {
+      if (event.touches.length !== 1) {
+        return;
+      }
+      event.preventDefault();
+      delta = event.touches[0].pageY - startPosition;
+    });
+    document.addEventListener('touchend', function (event) {
+      event.preventDefault();
+      if (Math.abs(delta) < 50) {
+        return;
+      }
+      if (delta < 0) {
+        next(slides);
+      } else {
+        previous(slides);
+      }
+    });
+    var wheeled;
+    document.addEventListener('mousewheel', function (event) {
+      event.preventDefault();
+      if (Math.abs(event.wheelDeltaY) < 50) {
+        return;
+      }
+      var wheelTime = new Date();
+      if (wheeled && wheelTime.getTime() - wheeled.getTime() < 1000) {
+        return;
+      }
+      if (event.wheelDeltaY < 0) {
+        next(slides);
+      } else {
+        previous(slides);
+      }
+      wheeled = wheelTime;
+    });
+  });
+})();
